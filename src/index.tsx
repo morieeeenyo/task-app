5d64ed19b4c32b707e087269a6fe34e52bb0505c
togo-mentor <uint2435@gmail.com>
2022-05-07 08:35:19 +0900

    render fixed

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "./index.css";
 import CognitoAuthProvider from "./cognito/CognitoAuthProvider";
 import awsconfig from "./aws-exports";
 
-ReactDOM.render(
-  <CognitoAuthProvider amplifyConfig={awsconfig}>
-    <App />
-  </CognitoAuthProvider>,
-  document.getElementById("root")
-);
+
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <CognitoAuthProvider amplifyConfig={awsconfig}>
+      <App />
+    </CognitoAuthProvider>
+  );
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
