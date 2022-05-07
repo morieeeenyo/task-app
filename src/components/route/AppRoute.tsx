6caee0aeee14d535d849d1ffd17ce48d2eeb7b80
togo-mentor <uint2435@gmail.com>
2022-05-08 06:41:13 +0900

    delete warning

--- src/components/route/AppRoute.tsx
+++ src/components/route/AppRoute.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+
 export const AppRoute = ({
   component: Component,
   layout: Layout,
--- src/components/route/ProtectedRoute.tsx
+++ src/components/route/ProtectedRoute.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Route, Navigate, Routes } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { AuthContext } from "../../cognito/AuthContext";
 import { Loading } from "../Loading";
 
